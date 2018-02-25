f1a39b146cbf5c209aae0e7a880b7b9fa8c2a046
valanikevin <valanikevin@gmail.com>
2018-02-26 01:36:16 +0530

    With added More Menu Items

--- app/src/main/java/com/aiora/trendy/App.java
+++ app/src/main/java/com/aiora/trendy/App.java
@@ -113,7 +113,7 @@ public class App extends Application {
 
     public void generateNotification(String message, String body, String answer) {
 
-        String title = "Quiz of the Day Answer";
+        String title = "Quiz of the Day Result:";
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             NotificationHelper notificationHelper = new NotificationHelper(this);
